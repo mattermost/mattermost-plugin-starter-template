a432c21b64ccf230741bf95af2f0c939851679c2
krjn <51929404+krjn@users.noreply.github.com>
2019-07-01 18:23:19 -0300

    [MM-11829] Add example of custom admin console for plugin

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,32 @@
 import {id as pluginId} from './manifest';
 
+import Settings from './components/settings.jsx';
+
+// eslint-disable-next-line no-unused-vars
+function addToAdminConsole(registry) {
+    registry.registerAdminConsolePlugin((adminDefinition) => {
+        // the settings will appear on the authentication section
+        adminDefinition.authentication.starter = {
+            url: 'plugins/starter',
+            icon: 'fa-rocket',
+            title: 'com.mattermost.starter-plugin.title',
+            title_default: 'Starter',
+            isHidden: () => false,
+            schema: {
+                id: 'StarterPlugin',
+                component: Settings,
+            },
+        };
+        return adminDefinition;
+    });
+}
+
 export default class Plugin {
     // eslint-disable-next-line no-unused-vars
     initialize(registry, store) {
         // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+
+        // addToAdminConsole(registry);
     }
 }
 
