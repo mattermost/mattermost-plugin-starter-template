c38a7117ad6e2c84e77491465fbf07d7ea267842
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2019-11-14 17:39:39 +0300

    build/manifest: print whole manifest variable as it is
    while generating manifest files for both JS and Go sources.
    
    fixes #72.
    
    * added new `vargen` package to do code generation for Go's runtime variables.
    
    * notice that the code generation package (github.com/dave/jennifer/jen) used in `vargen` package has two cons:
      * it reorders struct types' fields by alphabet instead of leaving it as it appears during struct's initialization.
      * it does not put new lines for evert struct field if struct is initialized with under 2 fields. this might be annoying for some.
    
    * we could fix cons described above by creating our own code generator that directly builds code by bare writing to a buffer or by using `go/ast` but both ways will add more code to maintain. and this might not make sense for some small advantage.
    
    * notice that generated Go variable may contain slice, array, map and struct values with no members. this is valid because they must have been initialized with no members. zero values on the other hand are discarded and won't appear in the generated code.
    
    * notice that generated JS variable may contain zero values. this is valid because code generation for JS made by using JSON marshaling and `encoding/json` removes empty values only when `omitempty` tag is used.
    
    * this commit introduces breaking changes since generated `manifest.js` and `manifest.go` files are now exporting `model.Manifest` type.
    
    * ran go mod tidy.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/blang/semver v3.6.1+incompatible // indirect
+	github.com/dave/jennifer v1.3.0
 	github.com/go-ldap/ldap v3.0.3+incompatible // indirect
 	github.com/hashicorp/go-hclog v0.9.2 // indirect
 	github.com/hashicorp/go-plugin v1.0.1 // indirect
--- server/manifest.go
+++ server/manifest.go
@@ -2,10 +2,19 @@
 
 package main
 
-var manifest = struct {
-	ID      string
-	Version string
-}{
-	ID:      "com.mattermost.plugin-starter-template",
-	Version: "0.1.0",
+import "github.com/mattermost/mattermost-server/model"
+
+var manifest = &model.Manifest{
+	Description:      "This plugin serves as a starting point for writing a Mattermost plugin.",
+	Id:               "com.mattermost.plugin-starter-template",
+	MinServerVersion: "5.12.0",
+	Name:             "Plugin Starter Template",
+	Server: &model.ManifestServer{Executables: &model.ManifestExecutables{
+		DarwinAmd64:  "server/dist/plugin-darwin-amd64",
+		LinuxAmd64:   "server/dist/plugin-linux-amd64",
+		WindowsAmd64: "server/dist/plugin-windows-amd64.exe",
+	}},
+	SettingsSchema: &model.PluginSettingsSchema{Settings: []*model.PluginSetting{}},
+	Version:        "0.1.0",
+	Webapp:         &model.ManifestWebapp{BundlePath: "webapp/dist/main.js"},
 }
