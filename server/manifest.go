8db2d85098ad877f4a253cd97b91f2d67834d798
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2019-11-20 13:53:18 +0300

    build/manifest: use JSON unmarshaling instead of code generation with vargen
    for the Go version.

--- server/manifest.go
+++ server/manifest.go
@@ -2,19 +2,40 @@
 
 package main
 
-import "github.com/mattermost/mattermost-server/model"
+import (
+	"strings"
 
-var manifest = &model.Manifest{
-	Description:      "This plugin serves as a starting point for writing a Mattermost plugin.",
-	Id:               "com.mattermost.plugin-starter-template",
-	MinServerVersion: "5.12.0",
-	Name:             "Plugin Starter Template",
-	Server: &model.ManifestServer{Executables: &model.ManifestExecutables{
-		DarwinAmd64:  "server/dist/plugin-darwin-amd64",
-		LinuxAmd64:   "server/dist/plugin-linux-amd64",
-		WindowsAmd64: "server/dist/plugin-windows-amd64.exe",
-	}},
-	SettingsSchema: &model.PluginSettingsSchema{Settings: []*model.PluginSetting{}},
-	Version:        "0.1.0",
-	Webapp:         &model.ManifestWebapp{BundlePath: "webapp/dist/main.js"},
+	"github.com/mattermost/mattermost-server/model"
+)
+
+var manifest *model.Manifest
+
+const manifestStr = `
+{
+ "id": "com.mattermost.plugin-starter-template",
+ "name": "Plugin Starter Template",
+ "description": "This plugin serves as a starting point for writing a Mattermost plugin.",
+ "version": "0.1.0",
+ "min_server_version": "5.12.0",
+ "server": {
+  "executables": {
+   "linux-amd64": "server/dist/plugin-linux-amd64",
+   "darwin-amd64": "server/dist/plugin-darwin-amd64",
+   "windows-amd64": "server/dist/plugin-windows-amd64.exe"
+  },
+  "executable": ""
+ },
+ "webapp": {
+  "bundle_path": "webapp/dist/main.js"
+ },
+ "settings_schema": {
+  "header": "",
+  "footer": "",
+  "settings": []
+ }
+}
+`
+
+func init() {
+	manifest = model.ManifestFromJson(strings.NewReader(manifestStr))
 }
