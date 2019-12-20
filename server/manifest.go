8cf303c943d0f191a87d1a5c14bb9e72124ab46a
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-12-21 01:01:40 +0300

    build/manifest: print whole manifest variable as it is (#73)
    while generating manifest files for both JS and Go sources.
    
    fixes #72.

--- server/manifest.go
+++ server/manifest.go
@@ -2,10 +2,40 @@
 
 package main
 
-var manifest = struct {
-	ID      string
-	Version string
-}{
-	ID:      "com.mattermost.plugin-starter-template",
-	Version: "0.1.0",
+import (
+	"strings"
+
+	"github.com/mattermost/mattermost-server/model"
+)
+
+var manifest *model.Manifest
+
+const manifestStr = `
+{
+  "id": "com.mattermost.plugin-starter-template",
+  "name": "Plugin Starter Template",
+  "description": "This plugin serves as a starting point for writing a Mattermost plugin.",
+  "version": "0.1.0",
+  "min_server_version": "5.12.0",
+  "server": {
+    "executables": {
+      "linux-amd64": "server/dist/plugin-linux-amd64",
+      "darwin-amd64": "server/dist/plugin-darwin-amd64",
+      "windows-amd64": "server/dist/plugin-windows-amd64.exe"
+    },
+    "executable": ""
+  },
+  "webapp": {
+    "bundle_path": "webapp/dist/main.js"
+  },
+  "settings_schema": {
+    "header": "",
+    "footer": "",
+    "settings": []
+  }
+}
+`
+
+func init() {
+	manifest = model.ManifestFromJson(strings.NewReader(manifestStr))
 }
