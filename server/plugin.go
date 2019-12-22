1c3014f90662e90975a850ec0f076af57edf34fa
Ben Schumacher <ben.schumacher@mattermost.com>
2019-12-22 16:23:55 +0100

    Bump Mattermost to 5.18 (#76)

--- server/plugin.go
+++ server/plugin.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/mattermost/mattermost-server/plugin"
+	"github.com/mattermost/mattermost-server/v5/plugin"
 )
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
