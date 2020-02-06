c212dd3c5eee5bf983d3193aa7cb90b6ae25bb22
Michael Kochell <mjkochell@gmail.com>
2020-02-06 07:08:37 -0500

    typo

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                     options: {
                         cacheDirectory: true,
 
-                        // Babel configuration is in .babelrc because jest requires it to be there.
+                        // Babel configuration is in babel.config.js because jest requires it to be there.
                     },
                 },
             },
