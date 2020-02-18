93131207a11c83315d2276124f55691cdf73d4cc
Domas Monkus <domas@monkus.lt>
2020-02-18 22:45:25 +0200

    Add usage message.

--- build/sync/main.go
+++ build/sync/main.go
@@ -14,6 +14,13 @@ import (
 
 func main() {
 	verbose := flag.Bool("verbose", false, "enable verbose output")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Update a pluging directory with /mattermost-plugin-starter-template/.\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "%s <plan.yml> <plugin_directory>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 	// TODO: implement proper command line parameter parsing.
 	if len(os.Args) != 3 {
