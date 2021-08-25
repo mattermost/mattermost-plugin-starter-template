a80b0661954e4f82b3562cfe55eb3ec6355fa569
Ben Schumacher <ben.schumacher@mattermost.com>
2021-08-25 09:29:26 +0200

    [MM-38075] Use mattermost-server/v6 (#157)

--- server/plugin.go
+++ server/plugin.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/mattermost/mattermost-server/v6/plugin"
 )
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
