efa38c5bc2a70117b32ef21c050846b3545c9e37
coltoneshaw <coltoneshaw@gmail.com>
2022-06-10 16:36:45 -0400

    fixed imports with TS

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,10 +1,9 @@
 import {Store, Action} from 'redux';
 
-import {GlobalState} from 'mattermost-redux/types/store';
+import {GlobalState} from '@mattermost/types/lib/store';
 
-import manifest from '@/manifest';
+import {manifest} from '@/manifest';
 
-// eslint-disable-next-line import/no-unresolved
 import {PluginRegistry} from '@/types/mattermost-webapp';
 
 export default class Plugin {
@@ -16,7 +15,7 @@ export default class Plugin {
 
 declare global {
     interface Window {
-        registerPlugin(id: string, plugin: Plugin): void
+        registerPlugin(pluginId: string, plugin: Plugin): void
     }
 }
 
