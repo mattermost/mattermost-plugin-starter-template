1d487c0959b6baed3c24884210d9540589612df7
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-05-17 11:06:44 -0400

    MessageHasBeenDeleted hook test

--- server/plugin.go
+++ server/plugin.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"sync"
 
+	"github.com/mattermost/mattermost-server/server/public/model"
 	"github.com/mattermost/mattermost-server/server/public/plugin"
 )
 
@@ -26,3 +27,38 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 }
 
 // See https://developers.mattermost.com/extend/plugins/server/reference/
+
+func (p *Plugin) MessageHasBeenDeleted(c *plugin.Context, deletedPost *model.Post) {
+	user, err := p.API.GetUser(deletedPost.UserId)
+	if err != nil {
+		p.API.LogError(
+			"Failed to query user",
+			"user_id", deletedPost.UserId,
+			"error", err.Error(),
+		)
+		return
+	}
+
+	channel, err := p.API.GetChannel(deletedPost.ChannelId)
+	if err != nil {
+		p.API.LogError(
+			"Failed to query channel",
+			"channel_id", deletedPost.ChannelId,
+			"error", err.Error(),
+		)
+		return
+	}
+
+	msg := fmt.Sprintf("MessageHasBeenDeleted: @%s, ~%s\n\n%s", user.Username, channel.Name, deletedPost.Message)
+	if _, err := p.API.CreatePost(&model.Post{
+		ChannelId: channel.Id,
+		Message:   msg,
+	}); err != nil {
+		p.API.LogError(
+			"Failed to post MessageHasBeenDeleted message",
+			"channel_id", channel.Id,
+			"user_id", user.Id,
+			"error", err.Error(),
+		)
+	}
+}
